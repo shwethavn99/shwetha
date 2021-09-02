53f85058f15b69dd7bd160375f50e15240ac36ba
shwethavn99 <shwethanandikol.xworkz@gmail.com>
2021-09-02 18:06:20 +0530

    added

---
+++ accessories.java
@@ -0,0 +1,12 @@
+class Accessories{
+public static void main(String a[]){
+String type=("bangles jumkhi necklace");
+int banglescost=60;
+int jumkhicost=80;
+int necklacecost=100;
+System.out.println(type);
+System.out.println(banglescost);
+System.out.println(jumkhicost);
+System.out.println(necklacecost);
+}
+}
