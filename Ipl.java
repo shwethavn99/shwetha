41b6417eeec6e2bb73cfebbc3c32d799819d91a5
shwethavn99 <shwethanandikol.xworkz@gmail.com>
2021-09-02 18:01:19 +0530

    added

---
+++ Ipl.java
@@ -0,0 +1,7 @@
+class Ipl{
+static String teams[]={"RCB","MI","CSK","KKR","RR","SRH","PBKS","DC"};
+public static void main(String a[]){
+for(int s=0;s<teams.length;s++)
+System.out.print(teams[s] +" ");
+}
+}
